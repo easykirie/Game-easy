7b295e3ae75a4d73ed82beec605b8078e700c734
Unknown <s2kmrs2@naver.com>
2018-03-23 14:06:27 +0900

    벽돌깨기

---
+++ BlockGame/Assets/Scripts/BlockCreate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCreate : MonoBehaviour {
+
+    public int MaximumCount;
+    public int MinimumCount;    
+    public Transform[] SpawnPosition;
+    public GameObject Ball;
+    int[] BlockPosition;
+    int CreateIndex;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        int count = Random.Range(MaximumCount, MinimumCount);
+
+        while(CreateIndex <= count)
+        {
+            Instantiate(Prefabs, transform.position, transform.rotation);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
