28888a9ed9496e6a759bae3ed3fbfef3a02c3fb0
Unknown <s2kmrs2@naver.com>
2018-03-28 11:23:09 +0900

    벽돌깨기 수정1

---
+++ BlockGame/Assets/Scripts/BallController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float BallSpeed = 10;
+    public Transform SpawnPosition;
+    public GameObject Ball;
+    public float ReviveCount;
+    public GameObject[] Players;
+
+    private void Awake()
+    {
+        GameObject obj = Instantiate(Ball, SpawnPosition.position, SpawnPosition.rotation);//게임오브젝트 변수 obj에 Ball프리팹을 SpawnPosition 위치랑 각도 저장
+
+        obj.transform.SetParent(SpawnPosition);//변수 obj의 부모로 SpawnPosition을 설정
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- BlockGame/Assets/Scripts/BlockCreate.cs
+++ BlockGame/Assets/Scripts/BlockCreate.cs
@@ -7,22 +7,47 @@ public class BlockCreate : MonoBehaviour {
     public int MaximumCount;
     public int MinimumCount;    
     public Transform[] SpawnPosition;
-    public GameObject Ball;
-    int[] BlockPosition;
-    int CreateIndex;
+    public GameObject Block;
+    List<int> BlockPosition = new List<int>();
+    int CreateIndex = 0;
 
     
 
     // Use this for initialization
     void Start () {
-        int count = Random.Range(MaximumCount, MinimumCount);
+        MaximumCount = 40;
+        MinimumCount = 20;
+
+        int count = Random.Range(MinimumCount, MaximumCount);
 
         while(CreateIndex <= count)
         {
-            Instantiate(Prefabs, transform.position, transform.rotation);
+            int SpawnIndex = Random.Range(0, SpawnPosition.Length);
+            bool CanCreate = true;
+
+            for(int i = 0;i < BlockPosition.Count; i++)
+            {
+                if(BlockPosition[i] == SpawnIndex)
+                {
+                    CanCreate = false;
+                    break;
+                }
+            }
+
+            if(CanCreate == true)
+            {
+                Instantiate(Block, SpawnPosition[SpawnIndex].position, SpawnPosition[SpawnIndex].rotation);
+
+                BlockPosition.Add(SpawnIndex);
+
+                CreateIndex++;
+            }
+            
         }
         
 	}
+
+    
 	
 	// Update is called once per frame
 	void Update () {
