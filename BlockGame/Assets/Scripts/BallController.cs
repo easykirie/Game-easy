96155389e64a2b54c5cc6fb35e0a38c43ad7e0b4
Unknown <s2kmrs2@naver.com>
2018-03-29 17:03:37 +0900

    벽돌깨기 수정2

--- BlockGame/Assets/Scripts/BallController.cs
+++ BlockGame/Assets/Scripts/BallController.cs
@@ -9,20 +9,39 @@ public class BallController : MonoBehaviour {
     public GameObject Ball;
     public float ReviveCount;
     public GameObject[] Players;
+    bool FirstCheck;
+    Rigidbody rigid;
 
     private void Awake()
+    {
+        BallCreate();
+    }
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        BallShoot();
+	}
+
+    void BallCreate()
     {
         GameObject obj = Instantiate(Ball, SpawnPosition.position, SpawnPosition.rotation);//게임오브젝트 변수 obj에 Ball프리팹을 SpawnPosition 위치랑 각도 저장
 
         obj.transform.SetParent(SpawnPosition);//변수 obj의 부모로 SpawnPosition을 설정
+
+        FirstCheck = false;//FirstCheck를 false로 바꿔서 Space바를 누르면 발사가능하게 해줌.
     }
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void BallShoot()//FirstCheck가 false고 Space바를 누르면 발사. FirstCheck를 true로 바꿔줌.
+    { 
+        if(FirstCheck == false && Input.GetKeyDown(KeyCode.Space))
+        {
+            FirstCheck = true;
+            rigid.AddForce(Vector3.up * BallSpeed, ForceMode.Impulse);           
+        }
+    }
+    
 }
