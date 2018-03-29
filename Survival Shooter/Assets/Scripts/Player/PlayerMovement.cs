4402e461c4413c8bfddae28f56288edf195c2717
Unknown <s2kmrs2@naver.com>
2018-03-29 14:35:51 +0900

    서바이벌 슈터겜 수업1번

--- Survival Shooter/Assets/Scripts/Player/PlayerMovement.cs
+++ Survival Shooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -14,7 +14,10 @@ public class PlayerMovement : MonoBehaviour {
 
     private void Awake()
     {
+        floorMask = LayerMask.GetMask("Floor");
+
         playerRigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Use this for initialization
@@ -29,6 +32,10 @@ public class PlayerMovement : MonoBehaviour {
         float v = Input.GetAxis("Vertical");
 
         Move(h, v);
+
+        Turning();
+
+        Animating(h, v);
     }
 
     void Move(float h, float v)
@@ -39,5 +46,28 @@ public class PlayerMovement : MonoBehaviour {
 
         playerRigid.MovePosition(transform.position + movement);
         
+    }
+    void Animating(float h, float v)
+    {
+        bool walking = (h != 0 || v != 0);
+
+        anim.SetBool("IsWalking", walking);
+    }
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if(Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+            playerRigid.MoveRotation(newRotation);
+        }
     }
 }
