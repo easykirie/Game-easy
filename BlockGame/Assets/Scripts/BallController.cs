e07188b59516c7c6b61f2bca70a0ccfc81615a65
Unknown <s2kmrs2@naver.com>
2018-03-30 16:47:57 +0900

    벽돌깨기 수정3

--- BlockGame/Assets/Scripts/BallController.cs
+++ BlockGame/Assets/Scripts/BallController.cs
@@ -11,6 +11,13 @@ public class BallController : MonoBehaviour {
     public GameObject[] Players;
     bool FirstCheck;
     Rigidbody rigid;
+    public Transform tr;//플레이어 위치.
+    public Transform b_tr;//공위치.
+    GameObject obj;
+
+    public GameObject Blcok;
+
+
 
     private void Awake()
     {
@@ -18,7 +25,7 @@ public class BallController : MonoBehaviour {
     }
     // Use this for initialization
     void Start () {
-        rigid = GetComponent<Rigidbody>();
+        //rigid = GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
@@ -27,21 +34,33 @@ public class BallController : MonoBehaviour {
 	}
 
     void BallCreate()
-    {
-        GameObject obj = Instantiate(Ball, SpawnPosition.position, SpawnPosition.rotation);//게임오브젝트 변수 obj에 Ball프리팹을 SpawnPosition 위치랑 각도 저장
-
+    {                
+        obj = Instantiate(Ball, SpawnPosition.position, SpawnPosition.rotation);//게임오브젝트 변수 obj에 Ball프리팹을 SpawnPosition 위치랑 각도 저장
         obj.transform.SetParent(SpawnPosition);//변수 obj의 부모로 SpawnPosition을 설정
-
+        rigid = obj.GetComponent<Rigidbody>();
         FirstCheck = false;//FirstCheck를 false로 바꿔서 Space바를 누르면 발사가능하게 해줌.
     }
 
     void BallShoot()//FirstCheck가 false고 Space바를 누르면 발사. FirstCheck를 true로 바꿔줌.
-    { 
-        if(FirstCheck == false && Input.GetKeyDown(KeyCode.Space))
+    {
+        if (FirstCheck == false && Input.GetKeyDown(KeyCode.Space))
         {
             FirstCheck = true;
-            rigid.AddForce(Vector3.up * BallSpeed, ForceMode.Impulse);           
+            obj.transform.SetParent(null);//부모 오브젝트에서 꺼냄(공을) 발사때만.
+            rigid.AddForce(Vector3.up * BallSpeed, ForceMode.Impulse);            
         }
     }
-    
+    void BlockBreack()//공이 부딪히면 블록 제거.
+    {
+        
+    }
+    private void OnTriggerEnter(Collider col)//볼 스크립트 따로 만들어서 넣음됨.
+    {
+        if(col.gameObject.tag == "Block")
+        {
+            Destroy(col.gameObject, 0.1f);
+        }
+    }
+
+
 }
---
+++ BlockGame/Assets/Scripts/BallDeath.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDeath : MonoBehaviour {
+
+    public float re;
+    public GameObject Ball;
+
+	// Use this for initialization
+	void Start () {
+        re = GetComponent<BallController>().ReviveCount;
+	}	
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Ball")
+        {
+            Destroy(Ball, 0.1f);
+        }
+    }
+}
